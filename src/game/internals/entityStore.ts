c1cb8cc95d58de5d2e8db8a75bc3101d8c5e9ade
Chamu Rajasekera <chamu.rajasekera@gmail.com>
2024-05-05 17:54:09 -0700

    store updates

--- src/game/internals/entityStore.ts
+++ src/game/internals/entityStore.ts
@@ -2,15 +2,41 @@ import type { Movable, Renderable, Component } from "./components";
 import { reverseObject } from "./utilities";
 
 const componentOrder: Component[] = ["movable", "renderable"] as const;
+type Entity = [Movable?, Renderable?];
+
 const componentOrderMap: { [key in Component]: number } = reverseObject(
   componentOrder
 ) as { [key in Component]: number };
-type Entity = [number, Movable?, Renderable?];
-type K = ["A", "B"];
+const componentMaxCount = componentOrder.length;
 
-let idCounter = 0;
 const entityStore: Entity[] = [];
 
 export const createEntity = (): Entity => {
-  return [idCounter++, undefined, undefined];
+  const entity = Array(componentMaxCount) as Entity;
+  // Entity's position in the entityStore array is its id.
+  entityStore.push(entity);
+  return entity;
 };
+
+export function* getComponentsOfType<T>(
+  componentType: Component
+): Generator<T> {
+  const componentIndex = componentOrderMap[componentType];
+  for (const entity of entityStore) {
+    if (entity[componentIndex]) {
+      yield entity[componentIndex] as T;
+    }
+  }
+}
+
+export function* getEntitiesWithComponent(
+  componentType: Component
+): Generator<{ id: number; entity: Entity }> {
+  const componentIndex = componentOrderMap[componentType];
+  for (let i = 0; i < entityStore.length; i++) {
+    const entity = entityStore[i];
+    if (entity[componentIndex]) {
+      yield { id: i, entity };
+    }
+  }
+}
